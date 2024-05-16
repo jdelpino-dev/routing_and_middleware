daa33050cf8a4863ebd8553732054c3081518a25
José Delpino <delpinoivivas@gmail.com>
2024-05-16 14:53:35 -0500

    feat: Add fakeDB module for in-memory data storage

---
+++ src/fakeDB.js
@@ -0,0 +1,11 @@
+/**
+ * Fake database module.
+ * This module exports an array to act as an in-memory data store
+ * for shopping list items.
+ *
+ * Note: This data will be cleared each time the server restarts.
+ */
+
+let items = [];
+
+export default items;
